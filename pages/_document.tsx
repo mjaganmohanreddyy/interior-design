4d2e41e8683e3e69aadfb711b11cbf7fdec838c4
rajashekar ks <rajksreddy@gmail.com>
2023-11-03 07:15:57 +0530

    done metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,42 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en" className="h-full bg-background" suppressHydrationWarning>
-      <Head>
-        <meta name="theme-color" content="#000000" />
-        <meta
-          name="description"
-          content="Interiorai: Interior Design Ideas Inspiration, and Virtual interior design By AI."
-        />
-        <meta property="og:title" content="Interiorai | Interior Design Ideas Inspiration " />
-        <meta
-          property="og:description"
-          content="Interiorai: Interior Design Ideas Inspiration, and Virtual interior design."
-        />
-        <meta
-          property="og:image"
-          content="/_static/meta-image.png"
-        />
-        <meta property="og:url" content="https://www.interiorai.online" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="_rajashekarks" />
-        <meta name="twitter:creator" content="_rajashekarks" />
-        <meta name="twitter:title" content="interiorai" />
-        <meta
-          name="twitter:description"
-          content="Interiorai: Interior Design Ideas Inspiration, and Virtual interior design ."
-        />
-        <meta
-          name="twitter:image"
-          content="https://www.interiorai.io/_static/meta-image.png"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body className="h-full">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    let description = "Design your own interior with the first AI Interior Designer.";
+    let ogimage = "https://www.interiorai.online";
+    let sitename = "interiorai.online";
+    let title = "interiorai";
+
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content={description} />
+          <meta property="og:site_name" content={sitename} />
+          <meta property="og:description" content={description} />
+          <meta property="og:title" content={title} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={title} />
+          <meta name="twitter:description" content={description} />
+          <meta property="og:image" content={ogimage} />
+          <meta name="twitter:image" content={ogimage} />
+        </Head>
+        <body className="bg-[#17181C] text-white">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
