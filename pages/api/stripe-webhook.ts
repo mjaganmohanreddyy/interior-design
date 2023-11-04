af4edc9e991198c87eb8edb791028a15a4be5f21
rajashekar ks <rajksreddy@gmail.com>
2023-11-04 21:00:13 +0530

    price-added

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -59,16 +59,16 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 
       // @ts-ignore
       switch (paymentIntent.amount_subtotal) {
-        case 500:
-        case 1000:
+        case 5000:
+        case 10000:
           creditAmount = 20;
           break;
-        case 1900:
-        case 3000:
+        case 25000:
+        case 30000:
           creditAmount = 100;
           break;
-        case 3500:
-        case 5000:
+        case 45000:
+        case 50000:
           creditAmount = 250;
           break;
         case 7000:
--- pages/buy-credits.tsx
+++ pages/buy-credits.tsx
@@ -40,8 +40,8 @@ export default function Pricing() {
         {session?.user?.email && (
           // @ts-ignore
           <stripe-pricing-table
-            pricing-table-id="prctbl_1MobnNK4W9ejG97elHjeFCEq"
-            publishable-key="pk_live_51HGpOvK4W9ejG97eYSm02d1hgagCOAAcKQCtH7258w6fA8wxo2PRv2xs2wSUG2xkV2YLBc0h3HxKITTFeJGtWai500o6bqGFHF"
+            pricing-table-id="prctbl_1O8lgXSBqFQm7EzRlVAZxMeh"
+            publishable-key="pk_live_51Ncj1WSBqFQm7EzRq9WZhmeYxkSKQ2IatSZcobjhEhrGslWboafEbGpKWSNoTENQoCCNXGeRdYh2TPKcqIbIgoc700pMNNuoOX"
             client-reference-id={session.user.email}
             customer-email={session.user.email}
           />
