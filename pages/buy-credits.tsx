a312e2f8bce33d9f9da589278b0208c29eeb0921
rajashekar ks <rajksreddy@gmail.com>
2023-11-05 20:56:57 +0530

    inr-price-done

--- pages/buy-credits.tsx
+++ pages/buy-credits.tsx
@@ -40,8 +40,8 @@ export default function Pricing() {
         {session?.user?.email && (
           // @ts-ignore
           <stripe-pricing-table
-            pricing-table-id="prctbl_1MobnNK4W9ejG97elHjeFCEq"
-            publishable-key="pk_live_51HGpOvK4W9ejG97eYSm02d1hgagCOAAcKQCtH7258w6fA8wxo2PRv2xs2wSUG2xkV2YLBc0h3HxKITTFeJGtWai500o6bqGFHF"
+            pricing-table-id="prctbl_1O97l2SJUtTwe8aBtRxaTQDp"
+            publishable-key="pk_live_51O96Z6SJUtTwe8aBO2iQgvAyZkMPNgmKCEmeOyPGou8L1CNJ38O06L8wk2IsRdjsB26cdvc5QQiEeSQqYgbpAD4i00XrTZBUXb"
             client-reference-id={session.user.email}
             customer-email={session.user.email}
           />
